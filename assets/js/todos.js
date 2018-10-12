f05d7d46eb2f82189994098c88d05a6d4afdc8f7
Mohamed Kanzi <mohammedalkanzi8@gmail.com>
2018-10-12 16:11:20 +0300

    Delete item from the list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,19 @@
+// ##################################################
+// Line through element and turn it to gray and vice virsa
+// ##################################################
 $("li").click(function() {
 	$(this).toggleClass("complete");
 });
+// ##################################################
+// Remove item from th to do list
+// ##################################################
+$("span").click(function(event) {
+	/* Stop propagation */
+	event.stopPropagation();
+	// select span to remove it
+	$(this).parent().fadeOut('500', function() {
+		$(this).remove();
+	});
+});
+// ##################################################
+// ##################################################
