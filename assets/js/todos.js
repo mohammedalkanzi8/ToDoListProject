c2282677d19702d41fc2c07c86ce51412c26844c
Mohamed Kanzi <mohammedalkanzi8@gmail.com>
2018-10-12 17:33:33 +0300

    Add elements and remove it from list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,19 +1,29 @@
 // ##################################################
 // Line through element and turn it to gray and vice virsa
 // ##################################################
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("complete");
 });
 // ##################################################
 // Remove item from th to do list
 // ##################################################
-$("span").click(function(event) {
-	/* Stop propagation */
-	event.stopPropagation();
+$("ul").on("click", "span", function(event) {
 	// select span to remove it
 	$(this).parent().fadeOut('500', function() {
 		$(this).remove();
 	});
+	/* Stop propagation */
+	event.stopPropagation();
 });
 // ##################################################
-// ##################################################
+// Add Value to the text input
+// #################################################
+$("input[type = 'text']").keypress(function(event) {
+	if (event.which === 13) {
+		/* Act on the event */
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append('<li><span>X</span> ' + todoText + '</li>');
+	};
+	
+});
